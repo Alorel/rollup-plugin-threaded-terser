83ed976e02c46e2e2bc5b8bd96a470be990baa6a
Arturas <a.molcanovas@gmail.com>
2020-09-20 19:29:39 +0100

    feat: Add support for Terser 5

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,12 @@
 import {ExistingRawSourceMap} from 'rollup';
-import {minify, MinifyOptions} from 'terser';
+import {minify, MinifyOptions, MinifyOutput} from 'terser';
 import {isMainThread, parentPort, workerData} from 'worker_threads';
+
+interface ExtendedMinifyOutput extends MinifyOutput {
+  error?: any;
+
+  warnings?: any[];
+}
 
 /** @internal */
 export interface WorkerInput {
@@ -19,29 +25,46 @@ export interface WorkerOutput {
 if (isMainThread) {
   process.emitWarning(new Error('rollup threaded terser plugin worker included on main thread'));
 } else {
-  setImmediate(() => {
+  /** @internal */
+  function isPromise<T = any>(input: any): input is Promise<T> {
+    return !!input && typeof input['then'] === 'function' && typeof input['catch'] === 'function';
+  }
+
+  /** @internal */
+  async function run(): Promise<WorkerOutput> {
     const wd = workerData as WorkerInput;
+    // 5.x returns a promise, 4.x is synchronous
+    const rawResult: ExtendedMinifyOutput | Promise<ExtendedMinifyOutput> = minify(wd.code, wd.config);
+    const terserResult = isPromise<ExtendedMinifyOutput>(rawResult) ? (await rawResult) : rawResult;
+    const {warnings, code, error, map} = terserResult;
 
-    try {
-      const terserResult = minify(wd.code, wd.config);
-      const {warnings, code, error, map} = terserResult;
-
-      if (warnings?.length) {
-        for (const warning of warnings) {
-          parentPort!.postMessage({warning});
-        }
+    // Terser 4.x-only
+    if (warnings?.length) {
+      for (const warning of warnings) {
+        parentPort!.postMessage({warning});
       }
-      if (error) {
-        parentPort!.postMessage({error});
-      } else {
-        const out: WorkerOutput = {code: code || ''};
-        if (map) {
-          out.map = typeof map === 'string' ? JSON.parse(map) : map as any;
-        }
-        parentPort!.postMessage(out);
-      }
-    } catch (error) {
-      parentPort!.postMessage({error});
     }
-  });
+
+    // Also 4.x-only
+    if (error) {
+      return Promise.reject(error);
+    }
+
+    const out: WorkerOutput = {code: code || ''};
+    if (map) {
+      out.map = typeof map === 'string' ? JSON.parse(map) : map as any;
+    }
+
+    return out;
+  }
+
+  run()
+    .then(
+      out => {
+        parentPort!.postMessage(out);
+      },
+      error => {
+        parentPort!.postMessage({error});
+      }
+    );
 }
